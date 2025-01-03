522737b6b2b9c9e5882ba22544986265fb59227f
dcc0 <ivangavr777@gmail.com>
2025-01-03 18:38:05 +0300

    Update test_ip.c

--- test_ip.c
+++ test_ip.c
@@ -4,58 +4,60 @@
 и завершаем программу*/
 
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <string.h>
 
-int test(char * ip) {
-if(system(ip)==0) {
-return 1;
+int test(char * ip_test) {
+  if (system(ip_test) == 0) {
+    return 1;
+  } else {
+    return 0;
+  }
 }
-else
-{
-return 0;
-}}
 
-void wait(int initial_state, char * ip, char * stable) {
-while (1) {
-sleep (5);
-test(ip);
-/*Если начальное состояние (b) не равно результату проверки ресурса - (test(ip)), - то
-подадим звуковой сигнал и изменим начальное состояние.*/
-if (initial_state !=test(ip)) {
-initial_state =test(ip);
-if (test(ip) == 1) {
-system("beep -f 900 -l 500; beep -f 700 -l 1000; beep -f 800 -l 500; beep -f 800 -l 500; beep -f 900 -l 1000;");
-printf("CETb ECTb!");
-if(stable == 0)
-break;
+void wait(int initial_state, char * ip_test, char * stable) {
+  while (1) {
+    sleep(5);
+    test(ip_test);
+    /*Если начальное состояние (b) не равно результату проверки ресурса - (test(ip)), - то
+    подадим звуковой сигнал и изменим начальное состояние.*/
+    if (initial_state != test(ip_test)) {
+      initial_state = test(ip_test);
+      if (test(ip_test) == 1) {
+        system("beep -f 900 -l 500; beep -f 700 -l 1000; beep -f 800 -l 500; beep -f 800 -l 500; beep -f 900 -l 1000;");
+        printf("CETb ECTb!");
+        if (stable == 0)
+          break;
+      } else {
+        system("beep -f 1200 -l 2000");
+      }
+    }
+  }
 }
-else
-{
-system("beep -f 1200 -l 2000");
-}}}}
 
 /*Первый запуск*/
-void start (char * ip) {
-if (test(ip)==0)
-system("beep -f 1200 -l 2000");
+void start(char * ip_test) {
+  if (test(ip_test) == 0)
+    system("beep -f 1200 -l 2000");
 }
 
+int main(int argc, char * argv[]) {
 
-int main (int argc, char *argv[]) {
+  if (argc < 2) {
+    printf("Введите адрес  вторым параметром\n\r");
+    printf("Третий параметр - 1 - бесконечное выполнение\n\r");
+    return 0;
+  }
 
-	if (argc < 2) {
-printf("Введите адрес  вторым параметром\n\r");
-printf("Третий параметр - 1 - бесконечное выполнение\n\r");
-return 0;
-}
+  int initial_state = 0;
+  char ip[100] = "ping -c 1 ";
+  char * stable = argv[2];
+  char ip_test[100];
 
-int initial_state = 0;
-char ip[100]="ping -c 1 ";
-char * stable=argv[2];
-
-/*Проверим доступность ресурса. Переменная initial_state хранит начальное состояние*/
-sprintf(ip, "%s%s", ip, argv[1]);
-start (ip);
-wait (initial_state, ip, stable);
+  /*Проверим доступность ресурса. Переменная initial_state хранит начальное состояние*/
+  sprintf(ip_test, "%s%s", ip, argv[1]);
+  start(ip_test);
+  wait(initial_state, ip_test, stable);
 }
